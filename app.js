341d3ac380bb10628f327f02e0e7b9a57dc6ce57
Xiaopi0 Studios <erikkirkegaardstage@gmail.com>
2021-03-03 17:24:08 +0100

    sfg

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+require('dotenv').config();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const app = express();
+const port = process.env.PORT || 5000;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully!");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: http://localhost:${port}`);
+})
